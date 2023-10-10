a4346d92a38cca276ebbbc6879bf4835daad54af
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-10-11 01:26:48 +0600

    xpsc

--- Codeforces/A_Sum_of_Three.cpp
+++ Codeforces/A_Sum_of_Three.cpp
@@ -16,20 +16,16 @@
 #define endl '\n'
 #define Tahsin ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 using namespace std;
-void solve()
-{
+void solve(){
     int n;
-    cin >> n;
+    cin>>n;
     vector<int> ans;
     bool flag = false;
 
-    for (int x = 1; x < n - 1; x++)
-    {
-        for (int y = 1; y < n - 1; y++)
-        {
+    for(int x = 1; x < n - 1; x++){
+        for(int y = 1; y < n - 1; y++){
             int z = n - x - y;
-            if ((x % 3 != 0) && (y % 3 != 0) && (z % 3 != 0) && (x != y && x != z && y != z) && (z > 0))
-            {
+            if((x % 3 != 0) and (y % 3 != 0) and (z % 3 != 0) and (x != y and x != z and y != z) and (z > 0)){
                 ans.push_back(x);
                 ans.push_back(y);
                 ans.push_back(z);
@@ -37,26 +33,25 @@ void solve()
                 break;
             }
         }
-        if (flag == true)
+        if(flag == true){
             break;
+        }
     }
-    if (flag == true)
-    {
+    if (flag == true){
         cout << "YES" << endl;
-        cout << ans[0] << " " << ans[1] << " " << ans[2] << endl;
+        cout<<ans[0]<<" "<<ans[1]<<" "<<ans[2]<<endl;
     }
-    else
-    {
-        cout << "NO" << endl;
+    else{
+        cout<<"NO"<<endl;
     }
 }
 
 int main()
 {
-    Tahsin int t;
-    cin >> t;
-    while (t--)
-    {
+    Tahsin
+    int t;
+    cin>>t;
+    while(t--){
         solve();
     }
     return 0;
