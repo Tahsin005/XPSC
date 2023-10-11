ab783154d1c566f81107fbeab465e31f7db386c1
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-10-12 01:27:41 +0600

    xpsc

--- Code Chef/Divisible_By_8.cpp
+++ Code Chef/Divisible_By_8.cpp
@@ -1,42 +1,53 @@
-// /******************************************************************/
-//  *                                                                 *
-//  *   |''||''|     |     ||     ||'   ;''''';.   '||'  '|.   '|'    *
-//  *      ||       |||    ||     ||   ||           ||    |'|   |     *
-//  *      ||      |  ||   |||||||||     \___       ||    | '|. |     *
-//  *      ||     .''''|.  ||     ||          \     ||    |   |||     *
-//  *      ||    .|.  .||. ||     ||.  ".......'   .||.  .|.   '|     *
-//  *                                                                 *
-// /******************************************************************/
+// Problem Link: https://www.codechef.com/problems/DIVISIBLEBY8
 
-#include<bits/stdc++.h>
-#define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define nl cout<<endl;
-#define endl '\n'
-#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+#include <bits/stdc++.h>
 using namespace std;
-void solve(){
-    int len;
-    string s;
-    cin>>len>>s;
 
-    if(len == 1){
-        cout<<8<<endl;
-    }
-    else if(len == 2){
-        int arr[] = {16,24,32,40,48,56,64,72,80,88,96};
-        int num = stoi(s);
-        if(num % 8 == 0) cout<<s<<endl;
-        
-    }
-}
-int main(){
-    Tahsin
+int main()
+{
+    int arr[10][10] = {
+        {8, 6, 4, 2, 0, 6, 4, 2, 0, 6},
+        {4, 2, 0, 6, 4, 2, 0, 6, 4, 2},
+        {8, 6, 4, 2, 0, 6, 4, 2, 0, 6},
+        {4, 2, 0, 6, 4, 2, 0, 6, 4, 2},
+        {8, 6, 4, 2, 0, 6, 4, 2, 0, 6},
+        {4, 2, 0, 6, 4, 2, 0, 6, 4, 2},
+        {8, 6, 4, 2, 0, 6, 4, 2, 0, 6},
+        {4, 2, 0, 6, 4, 2, 0, 6, 4, 2},
+        {8, 6, 4, 2, 0, 6, 4, 2, 0, 6},
+        {4, 2, 0, 6, 4, 2, 0, 6, 4, 2},
+    };
+
     int t;
-    cin>>t;
-    while(t--){
-        solve();
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+        int d1 = 0, d2 = 0, d3 = s[n - 1] - '0';
+        if (n > 1)
+            d2 = s[n - 2] - '0';
+        if (n > 2)
+            d1 = s[n - 3] - '0';
+
+        int target = arr[d1][d2];
+
+        if (target == 8 || target == 0)
+        {
+            if (n == 1)
+                cout << 8 << endl;
+            else if (d3 == 8 || d3 == 0)
+            {
+                cout << s << endl;
+            }
+            else
+            {
+                cout << s.substr(0, n - 1) << target << endl;
+            }
+        }
+        else
+            cout << s.substr(0, n - 1) << target << endl;
     }
-    return 0;    
+    return 0;
 }
