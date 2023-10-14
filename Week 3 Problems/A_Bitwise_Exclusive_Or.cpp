e1950aebbfe6d334160a1e98aab0a1ba75b90fb0
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-10-15 01:05:07 +0600

    xpsc

---
+++ Week 3 Problems/A_Bitwise_Exclusive_Or.cpp
@@ -0,0 +1,32 @@
+// /******************************************************************/
+//  *                                                                 *
+//  *   |''||''|     |     ||     ||'   ;''''';.   '||'  '|.   '|'    *
+//  *      ||       |||    ||     ||   ||           ||    |'|   |     *
+//  *      ||      |  ||   |||||||||     \___       ||    | '|. |     *
+//  *      ||     .''''|.  ||     ||          \     ||    |   |||     *
+//  *      ||    .|.  .||. ||     ||.  ".......'   .||.  .|.   '|     *
+//  *                                                                 *
+// /******************************************************************/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define nl cout<<endl;
+#define endl '\n'
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    cout<<(a ^ b)<<endl;
+}
+int main(){
+    Tahsin
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;    
+}
---
+++ Week 3 Problems/A_XORwice.cpp
@@ -0,0 +1,37 @@
+// /******************************************************************/
+//  *                                                                 *
+//  *   |''||''|     |     ||     ||'   ;''''';.   '||'  '|.   '|'    *
+//  *      ||       |||    ||     ||   ||           ||    |'|   |     *
+//  *      ||      |  ||   |||||||||     \___       ||    | '|. |     *
+//  *      ||     .''''|.  ||     ||          \     ||    |   |||     *
+//  *      ||    .|.  .||. ||     ||.  ".......'   .||.  .|.   '|     *
+//  *                                                                 *
+// /******************************************************************/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define nl cout<<endl;
+#define el '\n';
+#define vtr vector
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+void solve(){
+    ll a,b;
+    cin>>a>>b;
+    ll ans = a ^ b;
+    cout<<ans<<el;
+}
+int main(){
+    Tahsin
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;    
+}
