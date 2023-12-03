2135260744e10077cdf7087731df94fa22896f17
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-12-03 17:05:10 +0600

    xpsc

---
+++ Week 10 Problems/Q_Two_Permutations.cpp
@@ -0,0 +1,57 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+
+#include<bits/stdc++.h>
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+//@ Problem Link: 
+void solve(){
+     int n,a,b;
+    cin>>n>>a>>b;
+    if((n == a and n == b) or (n >= a and n >= b and n - b + 1 > a and n - b + 1 - a > 2)){
+        yes
+    }
+    else no
+}
+e4{
+    Tahsin
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;    
+}
