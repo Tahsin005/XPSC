38fc7145e0d8a78971f4564219a1bbe9d792282a
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-12-05 01:09:15 +0600

    xpsc

---
+++ Phitron Contest 8/B_Obtaining_the_String.cpp
@@ -0,0 +1,79 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+
+#include<bits/stdc++.h>
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+//@ Problem Link: 
+void solve(){
+    int n;
+	cin >> n;
+    string s,t;
+	cin >> s >> t;
+    vii v;
+    for(int i = 0; i < n; i++){
+		if(s[i] != t[i]){
+			int cnt = i;
+			while(s[cnt] != t[i] and cnt < n){
+				cnt++;
+			}
+			if(cnt >= n){
+                minus
+				return;
+			}
+			else{
+				for(int k = cnt - 1; k >= i; k--){
+					swap(s[k], s[k + 1]);
+					v.push_back(k + 1);
+				}
+			}
+		}
+	}
+	cout << v.size() << el;
+	for(auto val : v){
+		cout << val << " ";
+	}
+    
+}
+e4{
+    Tahsin
+    ll t = 1;
+    // cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;    
+}
---
+++ Week 10 Problems/D_1_Sage_s_Birthday_easy_version.cpp
@@ -0,0 +1,81 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+
+#include<bits/stdc++.h>
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+//@ Problem Link: 
+void solve(){
+    ll n;
+    cin >> n;
+    vii v(n + 1);
+    for(ll i = 1; i <= n; i++) cin >> v[i];
+
+    ll ans = 0;
+    ll idx = 1;
+    sort(all(v));
+    vii arr(n + 1,0);
+    for(ll i = 2; i <= n; i += 2){
+        arr[i] = v[idx];
+        idx++;
+    }
+
+    for(ll i = 1; i <= n; i++){
+        if(arr[i] == 0){
+            arr[i] = v[idx];
+            idx++;
+        }
+    }
+
+    for(ll i = 2; i < n; i += 2){
+        if(arr[i] < arr[i - 1] and arr[i] < arr[i + 1]){
+            ans++;
+        }
+    }
+    cout << ans << el;
+    for(ll i = 1; i <= n; i++){
+        cout << arr[i] << " ";
+    }
+}
+e4{
+    Tahsin
+    ll t = 1;
+    // cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;    
+}
