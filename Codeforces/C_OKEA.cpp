c859097540671607bd397a04b0d6b724a1f8fbef
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-12-06 20:27:37 +0600

    xpsc

---
+++ Codeforces/C_OKEA.cpp
@@ -0,0 +1,80 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+
+#include<bits/stdc++.h>
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+//@ Problem Link: 
+void solve(){
+    int n,k;
+    cin >> n >> k;
+
+    int has_even = (n * k) / 2;
+    int has_odd = (n * k) - has_even;
+
+    int req_even = k * (n / 2);
+    int req_odd = (n * k) - req_even;
+
+    if(has_even != req_even or has_odd != req_odd){
+        cno
+        return;
+    }
+    cyes
+    int odd = 1,even = 2;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= k; j++){
+            if(i & 1){
+                cout << odd << " ";
+                odd += 2;
+            }
+            else{
+                cout << even << " ";
+                even += 2;
+            }
+        }
+        nl
+    }
+    
+}
+e4{
+    Tahsin
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;    
+}
