98df601c7b8634026489a26c1ce27046c34e9a11
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-12-22 23:52:39 +0600

    xpsc

---
+++ Hola/Valid_Phone_Number.cpp
@@ -0,0 +1,115 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#else
+#define debug(x...)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#endif
+
+// @Problem Link: 
+void solve(){
+    string s;
+    cin >> s;
+    // cout << s.length() << el;
+    if(s.length() != 14){
+        cno
+    }
+    else{
+        if(s[0] == '+' and s[1] == '8' and s[2] == '8' and s[3] == '0' and s[4] == '1'){
+            if((s[5] != '0' and s[5] != '1' and s[5] != '2')){
+                // cyes 
+                for(int i = 1; i < s.length(); i++){
+                    if(s[i] < '0' or s[i] > '9'){
+                        cno 
+                        return;
+                    }
+                }
+                cyes
+            }
+            else{
+                cno
+                // messi
+            }
+        }
+        else{
+            cno 
+
+        }
+    }
+    
+
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
