0fa1629b261fc2c0f91fdd110cce2378c1490644
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-12-28 20:04:11 +0600

    xpsc

---
+++ Code Chef/Rock_Paper_Scissors.cpp
@@ -0,0 +1,114 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#else
+#define debug(x...)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#endif
+
+// @Problem Link: 
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    reverse(all(s));
+    string ans = "";
+    int minNeed = n / 2 + 1;
+    for(int i = 0; i < minNeed; i++){
+        if (s[i] == 'R') ans += 'P';
+        else if (s[i] == 'P') ans += 'S';
+        else ans += 'R';
+    }
+    int cnt = 0;
+    int need = 0;
+    for(int i = minNeed; i < n; i++){
+        if(s[i] == 'R'){
+            cnt++;
+        }
+        ans += 'P';
+        need++;
+    }
+    reverse(all(ans));
+    for(int i = need; i < n; i++){
+        if(ans[i] != 'P' and cnt > 0){
+            ans[i] = 'P';
+            cnt--;
+        }
+    }
+    cout << ans << el;
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ Week 13 Problems/I_Colorful_Table.cpp
@@ -0,0 +1,105 @@
+//                      ...              ...                                                             
+//    .::::::::.       .@@+              %@%                                                             
+//    =@@@@@@@@+       .@@+              +*+                                                             
+//    -**#@@%**-       .@@+                                                                              
+//       :@@-    :=+:  .@@+.==    .=+-   ::: ::: -+:                                                     
+//       :@@-   #@@@@% .@@#@@@%  +@@@@@: %@% #@##@@@-                                                    
+//       :@@-  +@@+*@@+.@@@+%@@=.@@+:%@% %@% #@@**@@%                                                    
+//       :@@-  :--  @@*.@@+ .@@*:@@= ::: %@% #@%  *@@                                                    
+//       :@@-   -*##@@*.@@+  @@* %@@@*-  %@% #@%  *@@                                                    
+//       :@@-  =@@#*@@*.@@+  @@*  +#@@@* %@% #@%  *@@                                                    
+//       :@@-  @@#  @@*.@@+  @@*:== .#@@ %@% #@%  *@@                                                    
+//       :@@-  %@@=*@@#.@@+  @@*-@@=:%@@ %@% #@%  *@@                                                    
+//       :@@-  -@@@%%@@.@@+  @@* *@@@@@- %@% #@%  *@@                                                    
+//       .--.   :+= :-- --.  :-:  :=+-.  :-: :-:  :--               
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Que Miras Bobo!" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#else
+#define debug(x...)
+#define HERE cerr << "here - " <<__LINE__ << "\n";
+#endif
+
+// @Problem Link: 
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vii a(n), b(k + 1);
+    for(auto &x : a){
+        cin >> x;
+        b[x] = 1;
+    }
+    int l = 0, r = n - 1;
+    for(int i = 1; i <= k; i++){
+        if(!b[i]){
+            cout << "0 ";
+            continue;
+        }
+        cout << (r - l + 1) * 2 << " ";
+        while(l <= r && a[l] <= i)
+            l++;
+        while(l <= r && a[r] <= i)
+            r--;
+    }
+    nl
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
