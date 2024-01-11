1bd81914917c71f2bc836daed9e97e54f5600f11
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-01-11 11:47:56 +0600

    xpsc

---
+++ B_Ugu.cpp
@@ -0,0 +1,126 @@
+/*
+        Author: tahsin_ferdous
+        Email: tahsin.ferdous3546@gmail.com
+
+        Qué mirás, bobo? Qué mirás, bobo?…
+*/          
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define fr(x, s, e, b) for (ll x = s; x <= e; x = x + b)
+#define rfr(x, s, e, b) for (ll x = s; x >= e; x = x - b)
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define pii pair<int,int>
+#define mii map<int,int>
+#define imax INT_MAX
+#define imin INT_MIN
+#define F first
+#define S second
+#define pb push_back
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Qué mirás, bobo?" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#else
+#define debug(x...)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#endif
+
+// @Problem Link: 
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    if (is_sorted(s.begin(), s.end())) {
+        cout << 0 << el;
+        return;
+    }
+
+    ll ans = 0;
+
+    if (s.back() == '0') {
+        int r = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '1') {
+                while (i > -1 and s[i] == '1') {
+                    swap(s[i], s[r]);
+                    i--;
+                    r--;
+                }
+                break;
+            }
+        }
+        ans++;
+    }
+    if (is_sorted(s.begin(), s.end())) {
+        cout << 1 << el;
+        return;
+    }
+    bool flag = false;
+    for (int i = n - 1; i >= 0; i--) {
+        if (flag) {
+            if (s[i] == '1' and (i == 0 or s[i - 1] == '0')) {
+                ans += 2;
+            }
+        }
+        else {
+            if (s[i] == '0') {
+                flag = true;
+            }
+        }
+    }
+    cout << ans << el;
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
