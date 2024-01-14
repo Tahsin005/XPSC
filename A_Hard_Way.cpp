47263811fadeeee459462b39b6585e97f52c5eac
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-01-14 19:24:16 +0600

    xpsc

---
+++ A_Hard_Way.cpp
@@ -0,0 +1,108 @@
+/*
+        Author: tahsin_ferdous
+        Email: tahsin.ferdous3546@gmail.com
+
+        Qué mirás, bobo? Qué mirás, bobo?…
+*/          
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define fr(x, s, e, b) for (ll x = s; x <= e; x = x + b)
+#define rfr(x, s, e, b) for (ll x = s; x >= e; x = x - b)
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define pii pair<int,int>
+#define mii map<int,int>
+#define imax INT_MAX
+#define imin INT_MIN
+#define F first
+#define S second
+#define pb push_back
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Qué mirás, bobo?" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#else
+#define debug(x...)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#endif
+
+// @Problem Link: 
+bool is_parallel_to_X(ll x1,ll y1,ll x2,ll y2){
+    return (y1>0 and y1==y2);
+}
+void solve(){
+    ll x1, y1;
+    cin >> x1 >> y1;
+    ll x2, y2;
+    cin >> x2 >> y2;
+    ll x3, y3;
+    cin >> x3 >> y3;
+
+    ll ans = 0;
+
+    if (is_parallel_to_X(x1, y1, x2, y2) and y3 < y1) {
+        ans += abs(x1 - x2);
+    }
+
+    if (is_parallel_to_X(x1, y1, x3, y3) and y2 < y1) {
+        ans += (abs(x1 - x3));
+    }
+
+    if (is_parallel_to_X(x2, y2, x3, y3) and y1 < y2) {
+        ans += abs(x2 - x3);
+    }
+
+    out(ans * 1LL);
+    nl
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ C_Partitioning_the_Array.cpp
@@ -0,0 +1,124 @@
+/*
+        Author: tahsin_ferdous
+        Email: tahsin.ferdous3546@gmail.com
+
+        Qué mirás, bobo? Qué mirás, bobo?…
+*/          
+#include<bits/stdc++.h>
+#define OJ
+using namespace std;
+#define ll long long
+#define cyes cout << "YES" << '\n';
+#define yes cout << "Yes" << '\n';
+#define cno cout << "NO"<< '\n';
+#define no cout << "No"<< '\n';
+#define minus cout << -1 << '\n';
+#define nl cout << '\n';
+#define el '\n'
+#define all(x) x.begin(), x.end()
+#define fr(x, s, e, b) for (ll x = s; x <= e; x = x + b)
+#define rfr(x, s, e, b) for (ll x = s; x >= e; x = x - b)
+#define vtr vector
+#define vii vector<int> 
+#define vll vector<ll>
+#define pii pair<int,int>
+#define mii map<int,int>
+#define imax INT_MAX
+#define imin INT_MIN
+#define F first
+#define S second
+#define pb push_back
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define messi cout << "Qué mirás, bobo?" << '\n';
+#define e4 int main()
+#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef OJ
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#else
+#define debug(x...)
+#define HERE cerr<<"here - "<<__LINE__<<"\n";
+#endif
+
+// @Problem Link:
+bool check(vii &v, int x, int m){
+    int n = v.size();
+    for (int i = x; i < n; i++) {
+        if (v[i % x] % m != v[i] % m) return false;
+    }
+    return true;
+}
+vii get_factors(int n){
+    vector<int> factors_;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            factors_.push_back(i);
+            if (i != n / i) {
+                factors_.push_back(n / i);
+            }
+        }
+    }
+    return factors_;
+}
+
+int checking(vector<int> &v){
+    int n = v.size();
+    if (v.size() == 1) return 1;
+    vii factors_ = get_factors(n);
+    int cnt = 0;
+    int nax = *max_element(v.begin(), v.end()) + 1;
+
+    for (auto x : factors_) {
+        bool flag = false;
+        for (int m = 2; m <= nax; m++) {
+            if (check(v, x, m)) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == true) cnt++;
+    }
+    return cnt;
+}
+void solve(){
+    int n; cin >> n;
+    vii v(n); for (int i = 0; i < n; i++) cin >> v[i];
+    cout << checking(v) << el;
+}
+e4{
+    #ifndef OJ
+        freopen("error.txt", "w", stderr);
+    #endif
+    #ifdef OJ 
+        Tahsin
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    for(ll cs = 1; cs <= t; cs++){
+        // cout << "Case #" << cs << ": ";
+        solve();
+    }
+    return 0;
+}
