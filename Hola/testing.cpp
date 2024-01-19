9f98a15fbd713f6efcdd7272be3071333e95895a
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-01-20 00:39:14 +0600

    solve

--- Hola/testing.cpp
+++ Hola/testing.cpp
@@ -3,10 +3,12 @@
 #define pi 3.14159
 using namespace std;
 int main(){
-    double a, area;
-    cin >> a;
-    area = pi * a * a;
-    cout << area << endl;
-    cout << fixed << setprecision(9) << area << endl;
+    int a,b;
+    cin >> a >> b;
+    float r;
+    r = a / b;
+    cout << floor(r) << endl;
+    cout << ceil(r) << endl;
+    cout << round(r) << endl;
     return 0;
 }
