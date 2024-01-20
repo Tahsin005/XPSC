f7f1dfd575c577e3793db6a16356c58fcc4a8bf1
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-01-20 22:16:31 +0600

    xpsc

--- Hola/testing.cpp
+++ Hola/testing.cpp
@@ -3,12 +3,16 @@
 #define pi 3.14159
 using namespace std;
 int main(){
-    int a,b;
-    cin >> a >> b;
-    float r;
-    r = a / b;
-    cout << floor(r) << endl;
-    cout << ceil(r) << endl;
-    cout << round(r) << endl;
+    char a; cin >> a;
+    int b;
+    b = int(a);
+    // cout << b << endl;
+    if ((b >= 65 and b <= 90) or (b >= 97 and b <= 122)) {
+        cout << "ALPHA" << endl;
+        if (b >= 65 and b <= 90) cout << "IS CAPITAL" << endl;
+        else if (b >= 97 and b <= 122) cout << "IS SMALL" << endl;
+    } else if (b >= 48 and b <= 57) {
+        cout << "IS DIGIT" << endl;
+    }
     return 0;
 }
