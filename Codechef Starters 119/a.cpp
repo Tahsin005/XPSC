0cbf737a99e874974f210a09633c299e9137f8fd
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-02-04 01:02:08 +0600

    day 132

---
+++ Codechef Starters 119/a.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    // int n; 
+    // cin >> n;
+    // if (n == 16 || n == 17 || n == 18) {
+    //     cout << "ADVITIYA" << endl;
+    // } else {
+    //     cout << "WAITING FOR ADVITIYA" << endl;
+    // }
+    // int arr[] = {1, 2, 3, 4};
+
+    // for (int i = 0; i < 5; i++) {
+    //     cout << arr[i] << endl;
+    // }
+
+    // string s = "hello";
+
+    // for (int i = 0; i < 10; i++) {
+    //     cout << s[i] << endl;
+    // }
+
+    stack<int> st;
+    // st.push(1);
+    // st.push(2);
+
+    st.pop();
+    st.pop();
+
+    st.pop();
+
+    return 0;
+}
