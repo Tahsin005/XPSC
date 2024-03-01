2833f9621cb723dd102518f7c0911d1c6b4a1b59
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-01 12:25:41 +0600

    day 158

---
+++ Week 17 Plus/B_Bad_Boy.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int tt; cin >> tt; while (tt--) {
+        int n, m, i, j; cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+    }
+    
+    return 0;
+}
