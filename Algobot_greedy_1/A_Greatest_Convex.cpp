697f186194385c4deefb3fd8708a826f5c666c11
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-01 04:31:00 +0600

    day 189

---
+++ Algobot_greedy_1/A_Greatest_Convex.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    unsigned long long tt; cin >> tt; while (tt--) {
+        unsigned long long k;
+        cin >> k;
+        cout << k - 1 << '\n';
+    }
+    
+    return 0;
+}
