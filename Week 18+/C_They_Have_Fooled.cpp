2f376c8dc4e78dd6a9647a587e0efb2758535e23
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-03 02:27:47 +0600

    day 191

---
+++ Week 18+/C_They_Have_Fooled.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if (n == 0) cout << 10 << '\n';
+    else if (n == 1) cout << 10 << '\n';
+    else if (n == 2) cout << 8 << '\n';
+    else if (n == 3) cout << 9 << '\n';
+    else if (n == 4) cout << 8 << '\n';
+    else if (n == 5) cout << 7 << '\n';
+    else if (n == 6) cout << 7 << '\n';
+    else if (n == 7) cout << 7 << '\n';
+    else if (n == 8) cout << 7 << '\n';
+    else if (n == 9) cout << 0 << '\n';
+    else if (n == 10) cout << 9 << '\n';
+    else if (n == 11) cout << 6 << '\n';
+    else if (n == 12) cout << 8 << '\n';
+    
+    return 0;
+}
