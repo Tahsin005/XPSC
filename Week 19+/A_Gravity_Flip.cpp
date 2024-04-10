507b03ffc842a7e146e95dba4fd90ad793c921d5
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-11 01:11:46 +0600

    day 199

---
+++ Week 19+/A_Gravity_Flip.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++) cout << v[i] << " ";
+    
+    return 0;
+}
