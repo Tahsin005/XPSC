a1b175075604c50ec709c9b69a668a4ff11ead7b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-05-02 01:22:36 +0600

    day 220:

---
+++ D_1_Reverse_Card_Easy_Version.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int tt; cin >> tt; while (tt--) {
+        ll n, m; cin >> n >> m;
+        ll cnt = 0;
+        for (ll i = 1; i <= m; i++) {
+            cnt += (n + i) / i / i;
+        }
+        cout << cnt - 1 << "\n";
+    }
+    
+    return 0;
+}
