5da09ed26038c1577c611cc312a009269be5bbd9
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-30 21:46:48 +0600

    day 279

---
+++ Week 22+/Yoga_Day.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void solve(){
+    int N; 
+    cin >> N;
+    cout <<  N / 12 << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    solve();
+}
