d10cb5197c9088ee8b4ace02cd6628b246239d11
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-08-13 23:51:43 +0600

    python practice problems

---
+++ Python Problem Solving/pr_1.py
@@ -0,0 +1,4 @@
+print("Recently I heard that you’ve achieved 95% marks in your exam. ")
+print("This is brilliant!")
+print("I wish you’ll shine in your life!", end="\t")
+print("Good luck with all the barriers(/\\) in your life.")
---
+++ Python Problem Solving/pr_5.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+if a < 5000:
+    print("something")
+elif a >= 5000 and a < 10000:
+    print("Levis Bag")
+elif a >= 10000:
+    print("Gucci Bag")
+    if a > 20000:
+        print("Gucci Belt")
+        
