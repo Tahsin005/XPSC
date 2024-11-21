0c1e4a0dd27c986790cecb7f190c6f0aee1fe1c7
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-21 13:31:15 +0600

    dfs cycle detection

--- ALGO PRACTICE/4_Connected_Components_in_a_Graph.cpp
+++ ALGO PRACTICE/4_Connected_Components_in_a_Graph.cpp
@@ -10,19 +10,24 @@ using namespace std;
 3 5
 6 7
 
-https://www.hackerearth.com/problem/algorithm/connected-components-in-a-graph/?purpose=login&source=problem-page&update=google
 
 
 */
 const int N = 1e5 + 10;
 vector<int> g[N];
 bool visited[N];
+
+vector<vector<int>> connected_components;
+vector<int> current_connected_components;
 void dfs(int vertex) {
     visited[vertex] = true;
+    current_connected_components.push_back(vertex);
     for (int child: g[vertex]) {
         if (visited[child]) continue;
         dfs(child);
     }
+
+
 }
 
 
@@ -41,12 +46,21 @@ int main(){
 
     for (int i = 1; i <= n; i++) {
         if (!visited[i]) {
+            current_connected_components.clear();
             dfs(i);
+            connected_components.push_back(current_connected_components);
             cnt++;
         }
     }
 
     cout << cnt << endl;
+
+    for (vector<int> cc: connected_components) {
+        for (int v: cc) {
+            cout << v << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
--- ALGO PRACTICE/5_DFS_cycle_detection.cpp
+++ ALGO PRACTICE/5_DFS_cycle_detection.cpp
@@ -1,65 +1,67 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define nl cout<<'\n';
-#define el '\n'
-#define vtr vector
-#define vii vector<int> 
-#define vll vector<ll>
-#define imax INT_MAX
-#define imin INT_MIN
-#define pb push_back
-#define pii pair<int,int>
-#define out(x) cout<<x<<"\n";
-#define out2(x,y) cout<<x<<" "<<y<<"\n";
-#define messi cout<<"Que Miras Bobo!"<<'\n';
-#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
 using namespace std;
-
-
-const int N = 1e5 + 7;
-vtr<int> graph[N];
-bool vis[N];
 /*
+
+Does not exists
 8 5
 1 2
 2 3
 2 4
 3 5
 6 7
+
+Exists
+8 6
+1 2
+2 3
+2 4
+3 5
+6 7
+1 5
+
+
 */
-bool dfs(int vertex,int par){
-    vis[vertex] = true;
-
-    bool isLoopExist = false;
-    for(int child: graph[vertex]){
-        if(vis[child] and child == par) continue;
-        if(vis[child]) return true;
-
-        isLoopExist |= dfs(child,vertex);
+const int N = 1e5 + 10;
+vector<int> g[N];
+bool visited[N];
+bool dfs(int vertex, int parent) {
+    visited[vertex] = true;
+    bool isLoopExists = false;
+    for (int child: g[vertex]) {
+        if (visited[child] and child == parent) continue;
+        if (visited[child]) return true;
+        isLoopExists |= dfs(child, vertex);
     }
 
-    return isLoopExist;
+    return isLoopExists;
 }
+
+
 int main(){
-    int n,m;
-    cin>>n>>m;
-    for(int i = 0; i < m; i++){
-        int a,b;
-        cin>>a>>b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
     }
-    bool flag = false;
-    for(int i = 1; i <= n; i++){
-        if(vis[i]) continue;
-        if(dfs(i,0)){
-            flag = true;
-            break;
+
+    bool exists = false;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            if (dfs(i, 0)) {
+                exists = true;
+                break;
+            }
         }
     }
 
-    cout<<flag<<el;
-    return 0;    
+    cout << exists << endl;
+
+    return 0;
 }
+
