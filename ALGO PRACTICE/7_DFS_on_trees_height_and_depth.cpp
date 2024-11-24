5f8a3d0387747616be27c495c20cada28dd8876d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-25 00:16:56 +0600

    height and depth of a tree using dfs

--- ALGO PRACTICE/7_DFS_on_trees_height_and_depth.cpp
+++ ALGO PRACTICE/7_DFS_on_trees_height_and_depth.cpp
@@ -1,22 +1,10 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define nl cout<<'\n';
-#define el '\n'
-#define vtr vector
-#define vii vector<int> 
-#define vll vector<ll>
-#define imax INT_MAX
-#define imin INT_MIN
-#define pb push_back
-#define pii pair<int,int>
-#define out(x) cout<<x<<"\n";
-#define out2(x,y) cout<<x<<" "<<y<<"\n";
-#define messi cout<<"Que Miras Bobo!"<<'\n';
-#define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
 using namespace std;
-/* 
+/*
+
+Input:
+
 13
 1 2
 1 3
@@ -25,50 +13,67 @@ using namespace std;
 3 4
 5 6
 5 7
-5 8 
+5 8
 8 12
 4 9
 4 10
 10 11
+
+Output:
+Node: 1, Depth: 0, Height: 4
+Node: 2, Depth: 1, Height: 3
+Node: 3, Depth: 1, Height: 3
+Node: 4, Depth: 2, Height: 2
+Node: 5, Depth: 2, Height: 2
+Node: 6, Depth: 3, Height: 0
+Node: 7, Depth: 3, Height: 0
+Node: 8, Depth: 3, Height: 1
+Node: 9, Depth: 3, Height: 0
+Node: 10, Depth: 3, Height: 1
+Node: 11, Depth: 4, Height: 0
+Node: 12, Depth: 4, Height: 0
+Node: 13, Depth: 1, Height: 0
+
+
+
 */
+const int N = 1e5 + 10;
+vector<int> g[N];
+vector<int> depth(N), height(N);
 
-const int N = 1e5;
-
-vtr<int> g[N];
-int height[N],depth[N];
-
-
-void dfs(int vertex,int par){
+void dfs(int vertex, int parent = 0) {
     // Take action on vertex after entering the vertex
-    for(int child: g[vertex]){
-        // Take action on child node before entering the child node
-        if(child == par) continue;
+    // cout << vertex << endl;
+    for (int child: g[vertex]) {
+        // cout << "Parent: " << vertex << ", child: " << child << endl;
+
+        if (child == parent) continue;
+        // Take action on child befor entering the child node
         depth[child] = depth[vertex] + 1;
-        dfs(child,vertex);
-        // Take action on child node after exiting the child node
+        dfs(child, vertex);
+        // Take action on child after exiting the child node
         height[vertex] = max(height[vertex], height[child] + 1);
     }
     // Take action on vertex before exiting the vertex
 }
-/*
-    Depth is calculated from the top
-    Height is calculated from the bottom(when exiting the child node)
-*/
+
 
 int main(){
     int n;
-    cin>>n;
-    for(int i = 0; i < n - 1; i++){
-        int v1,v2;
-        cin>>v1>>v2;
+    cin >> n;
 
+    for (int i = 0; i < n - 1; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
         g[v1].push_back(v2);
         g[v2].push_back(v1);
     }
 
-    dfs(1,0);
-    for(int i = 1; i <= n; i++){
-        out2(depth[i],height[i])
+    dfs(1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Node: " << i << ", Depth: " << depth[i] << ", Height: " << height[i] << endl;
     }
-    return 0;    
+    return 0;
 }
+
