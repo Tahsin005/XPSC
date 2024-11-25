57184695964c622d3fe43c1383ede33faf3b3b4a
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-25 17:53:15 +0600

    diameter of a tree

--- ALGO PRACTICE/9_Diameter_of_a_tree.cpp
+++ ALGO PRACTICE/9_Diameter_of_a_tree.cpp
@@ -1,22 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define nl cout<<'\n';
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nl cout << '\n';
 #define el '\n'
 #define vtr vector
-#define vii vector<int> 
+#define vii vector<int>
 #define vll vector<ll>
 #define imax INT_MAX
 #define imin INT_MIN
 #define pb push_back
-#define pii pair<int,int>
-#define out(x) cout<<x<<"\n";
-#define out2(x,y) cout<<x<<" "<<y<<"\n";
-#define messi cout<<"Que Miras Bobo!"<<'\n';
+#define pii pair<int, int>
+#define out(x) cout << x << "\n";
+#define out2(x, y) cout << x << " " << y << "\n";
+#define messi cout << "Que Miras Bobo!" << '\n';
 #define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
 using namespace std;
-/* 
+/*
 14
 1 2
 1 3
@@ -25,12 +25,15 @@ using namespace std;
 3 4
 5 6
 5 7
-5 8 
+5 8
 8 12
 4 9
 4 10
 10 11
 12 14
+
+Depth of the tree is : 9
+
 */
 
 const int N = 1e5;
@@ -39,16 +42,12 @@ vtr<int> g[N];
 int depth[N];
 
 
-void dfs(int vertex,int par){
-    // Take action on vertex after entering the vertex
-    for(int child: g[vertex]){
-        // Take action on child node before entering the child node
+void dfs(int vertex,int par) {
+    for (int child: g[vertex]) {
         if(child == par) continue;
         depth[child] = depth[vertex] + 1;
-        dfs(child,vertex);
-        // Take action on child node after exiting the child node
+        dfs(child, vertex);
     }
-    // Take action on vertex before exiting the vertex
 }
 /*
     1. With any node as root, find max depth node;
@@ -56,12 +55,12 @@ void dfs(int vertex,int par){
        will get the diameter of the tree
 */
 
-int main(){
+int main() {
     int n;
     cin>>n;
-    for(int i = 0; i < n - 1; i++){
-        int v1,v2;
-        cin>>v1>>v2;
+    for (int i = 0; i < n - 1; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
 
         g[v1].push_back(v2);
         g[v2].push_back(v1);
@@ -71,23 +70,25 @@ int main(){
 
     int mx_depth = -1;
     int mx_depth_node = -1;
-    for(int i = 1; i <= n; i++){
-        if(mx_depth < depth[i]){
+    for (int i = 1; i <= n; i++) {
+        if (mx_depth < depth[i]) {
             mx_depth = depth[i];
             mx_depth_node = i;
         }
     }
+
     // cout<<mx_depth<<" "<<mx_depth_node<<el;
 
-    memset(depth,0,sizeof(depth));
+    memset(depth, 0, sizeof(depth));
 
     dfs(mx_depth_node, -1);
     int finalMx_depth = -1;
-    for(int i = 1; i <= n; i++){
-        if(finalMx_depth < depth[i]){
+    for (int i = 1; i <= n; i++) {
+        if (finalMx_depth < depth[i]) {
             finalMx_depth = depth[i];
         }
     }
-    cout<<"Depth of the tree is : "<<finalMx_depth<<el;
-    return 0;    
+    cout << "Depth of the tree is : " << finalMx_depth << el;
+
+    return 0;
 }
--- ALGO PRACTICE/temp.cpp
+++ ALGO PRACTICE/temp.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define nl cout<<'\n';
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define nl cout << '\n';
 #define el '\n'
 #define vtr vector
 #define vii vector<int>
@@ -10,14 +10,14 @@
 #define imax INT_MAX
 #define imin INT_MIN
 #define pb push_back
-#define pii pair<int,int>
-#define out(x) cout<<x<<"\n";
-#define out2(x,y) cout<<x<<" "<<y<<"\n";
-#define messi cout<<"Que Miras Bobo!"<<'\n';
+#define pii pair<int, int>
+#define out(x) cout << x << "\n";
+#define out2(x, y) cout << x << " " << y << "\n";
+#define messi cout << "Que Miras Bobo!" << '\n';
 #define Tahsin ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
 using namespace std;
 /*
-13
+14
 1 2
 1 3
 1 13
@@ -30,58 +30,69 @@ using namespace std;
 4 9
 4 10
 10 11
+12 14
+
+Depth of the tree is : 9
+
 */
 
 const int N = 1e5;
-/*
-    Given Q queries, Q <= 1e5
-    In each given query V,
-    Print subtree sum of V , and number of even elements
-    in subtree of V
-*/
+
 vtr<int> g[N];
+int depth[N];
 
-int subtree_sum[N];
-int even_cnt[N];
-void dfs(int vertex,int par = 0){
+
+void dfs(int vertex,int par) {
     // Take action on vertex after entering the vertex
-    if(vertex % 2 ==0) even_cnt[vertex]++;
-
-    subtree_sum[vertex] += vertex;
-
-    for(int child: g[vertex]){
+    for (int child: g[vertex]) {
         // Take action on child node before entering the child node
         if(child == par) continue;
-        dfs(child,vertex);
+        depth[child] = depth[vertex] + 1;
+        dfs(child, vertex);
         // Take action on child node after exiting the child node
-        subtree_sum[vertex] += subtree_sum[child];
-
-        even_cnt[vertex] += even_cnt[child];
     }
     // Take action on vertex before exiting the vertex
 }
-int main(){
+/*
+    1. With any node as root, find max depth node;
+    2. With that node as root, find the max depth node / distance and
+       will get the diameter of the tree
+*/
+
+int main() {
     int n;
     cin>>n;
-    for(int i = 0; i < n - 1; i++){
-        int v1,v2;
-        cin>>v1>>v2;
+    for (int i = 0; i < n - 1; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
 
         g[v1].push_back(v2);
         g[v2].push_back(v1);
     }
-    dfs(1);
 
-    for(int i = 1; i <= n; i++){
-        cout<<subtree_sum[i]<<" "<<even_cnt[i]<<el;
+    dfs(1,-1);
+
+    int mx_depth = -1;
+    int mx_depth_node = -1;
+    for (int i = 1; i <= n; i++) {
+        if (mx_depth < depth[i]) {
+            mx_depth = depth[i];
+            mx_depth_node = i;
+        }
     }
-    // int q;
-    // cin>>q;
-    // while(q--){
-    //     int v;
-    //     cin>>v;
-    //     cout<<subtree_sum[v]<<" "<<even_cnt[v]<<el;
-    // }
 
+    // cout<<mx_depth<<" "<<mx_depth_node<<el;
+
+    memset(depth, 0, sizeof(depth));
+
+    dfs(mx_depth_node, -1);
+    int finalMx_depth = -1;
+    for (int i = 1; i <= n; i++) {
+        if (finalMx_depth < depth[i]) {
+            finalMx_depth = depth[i];
+        }
+    }
+    cout << "Depth of the tree is : " << finalMx_depth << el;
+    
     return 0;
 }
