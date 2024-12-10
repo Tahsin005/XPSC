35bcc806d865784a816c056ce491395284c18bd3
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-12-10 19:16:17 +0600

    day - 121

--- test.cpp
+++ test.cpp
@@ -1,76 +1,11 @@
-#include <stdio.h>
+void TEST_CASE(){
+    int sx, sy, ex, ey;
+    cin >> sx >> sy >> ex >> ey;
 
-void quicksort(int number[10], int first, int last)
-{
-
-    int i, j, pivot, temp;
-
-    if (first < last)
-    {
-
-        pivot = first;
-
-        i = first;
-
-        j = last;
-
-        while (i < j)
-        {
-
-            while (number[i] <= number[pivot] && i < last)
-
-                i++;
-
-            while (number[j] > number[pivot])
-
-                j--;
-
-            if (i < j)
-            {
-
-                temp = number[i];
-
-                number[i] = number[j];
-
-                number[j] = temp;
-            }
-        }
-
-        temp = number[pivot];
-
-        number[pivot] = number[j];
-
-        number[j] = temp;
-
-        quicksort(number, first, j - 1);
-
-        quicksort(number, j + 1, last);
+    if (ey >= sy and sx - sy >= ex - ey) {
+        cout << (ey - sy) + (sx + ey - sy - ex) << el;
+        return;
     }
+
+    return void(cout << "-1" << el);
 }
-
-int main()
-{
-
-    int i, count, number[10];
-
-    printf("Enter some elements (Max. - 10): ");
-
-    scanf("%d", &count);
-
-    printf("Enter %d elements: ", count);
-
-    for (i = 0; i < count; i++)
-
-        scanf("%d", &number[i]);
-
-    quicksort(number, 0, count - 1);
-
-    printf("The Sorted Order is: ");
-
-    for (i = 0; i < count; i++)
-
-        printf(" %d", number[i]);
-
-    return 0;
-}
-
